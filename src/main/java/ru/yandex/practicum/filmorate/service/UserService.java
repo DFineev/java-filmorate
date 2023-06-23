d2e6935d618493423119442996dc7519ffd6657a
DFineev <fineev.mitya@yandex.ru>
2023-06-24 01:18:40 +0300

    ready for review 1 Final Task 10

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -14,55 +14,74 @@ import java.util.*;
 public class UserService {
 
     private final UserStorage userStorage;
-public void makeFriends(int id, int friendId){
-User user1 = userStorage.getUserById(id);
-User user2 = userStorage.getUserById(friendId);
-Set<Integer> friends1 = user1.getFriends();
 
-  if (friends1 == null) {
-      friends1 = new HashSet<>();
-  }
-friends1.add(user2.getId());
-user1.setFriends(friends1);
+    public void makeFriends(int id, int friendId) {
+        User user1 = userStorage.getUserById(id);
+        User user2 = userStorage.getUserById(friendId);
+        Set<Integer> friends1 = user1.getFriends();
 
-Set<Integer> friends2 = user2.getFriends();
-    if (friends2 == null) {
-      friends2 = new HashSet<>();
+        if (friends1 == null) {
+            friends1 = new HashSet<>();
+        }
+        friends1.add(user2.getId());
+        user1.setFriends(friends1);
+
+        Set<Integer> friends2 = user2.getFriends();
+        if (friends2 == null) {
+            friends2 = new HashSet<>();
+        }
+        friends2.add(user1.getId());
+        user2.setFriends(friends2);
     }
-    friends2.add(user1.getId());
-    user2.setFriends(friends2);
- }
 
-public void deleteFromFriends(int id, int friendId){
-    User user1 = userStorage.getUserById(id);
-    User user2 = userStorage.getUserById(friendId);
-    Set<Integer> friends1 = user1.getFriends();
-    friends1.remove(user2.getId());
-    user1.setFriends(friends1);
-    Set<Integer> friends2 = user2.getFriends();
-    friends2.remove(user1.getId());
-    user2.setFriends(friends2);
-}
-public Set<User> getFriendsList(int id){
-    User user = userStorage.getUserById(id);
-    Set<Integer> friendIds = user.getFriends();
-    Set<User> friendsList = new HashSet<>();
-    for (Integer friendId : friendIds) {
-        friendsList.add(userStorage.getUserById(friendId));
+    public void deleteFromFriends(int id, int friendId) {
+        User user1 = userStorage.getUserById(id);
+        User user2 = userStorage.getUserById(friendId);
+        Set<Integer> friends1 = user1.getFriends();
+        friends1.remove(user2.getId());
+        user1.setFriends(friends1);
+        Set<Integer> friends2 = user2.getFriends();
+        friends2.remove(user1.getId());
+        user2.setFriends(friends2);
     }
-    return friendsList;
-}
-public Set<User> getCommonFriendsList(int id, int otherId){
-    Set<User> commonFriendsList = new HashSet<>();
-    Set<User> friends1List = getFriendsList(id);
-    Set<User> friends2List = getFriendsList(otherId);
-    for (User user : friends1List) {
-        for (User user1 : friends2List) {
-            if (user.getId()==user1.getId()) {
-                commonFriendsList.add(user);
+
+    public List<User> getFriendsList(Integer id) {
+        User user = userStorage.getUserById(id);
+        List<User> friendsList = new ArrayList<>();
+        UsersComparator usersComparator = new UsersComparator();
+        Set<Integer> friendIds = user.getFriends();
+        if (friendIds == null) {
+            return friendsList;
+        } else {
+            for (Integer friendId : friendIds) {
+                friendsList.add(userStorage.getUserById(friendId));
+            }
+            friendsList.sort(usersComparator);
+        }
+        return friendsList;
+    }
+
+    public List<User> getCommonFriendsList(int id, int otherId) {
+        List<User> commonFriendsList = new ArrayList<>();
+        UsersComparator usersComparator = new UsersComparator();
+        List<User> friends1List = getFriendsList(id);
+        List<User> friends2List = getFriendsList(otherId);
+        for (User user : friends1List) {
+            for (User user1 : friends2List) {
+                if (user.getId() == user1.getId()) {
+                    commonFriendsList.add(user);
+                }
             }
         }
+        commonFriendsList.sort(usersComparator.reversed());
+        return commonFriendsList;
     }
-    return commonFriendsList;
+
+    public static class UsersComparator implements Comparator<User> {
+        @Override
+        public int compare(User user1, User user2) {
+            return Integer.compare(user1.getId(), user2.getId());
+        }
+    }
 }
-}
+
