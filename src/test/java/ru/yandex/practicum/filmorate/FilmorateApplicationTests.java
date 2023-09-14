6e24fec8d37c1bad6b87687a23cb1a122e365664
DFineev <fineev.mitya@yandex.ru>
2023-09-14 21:56:59 +0300

    add all features, without tests

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,12 +2,20 @@ package ru.yandex.practicum.filmorate;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import ru.yandex.practicum.filmorate.adapter.DurationAdapter;
 import ru.yandex.practicum.filmorate.adapter.LocalDateAdapter;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.DAO.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.DAO.UserDbStorage;
 
 import java.io.IOException;
 import java.net.URI;
@@ -16,13 +24,40 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.Random;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class FilmorateApplicationTests {
-    private static final int PORT = 8080;
+//@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@AutoConfigureTestDatabase
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DataJdbcTest
+@Sql(value = {"/schematest.sql", "/datatest.sql"})
+
+public class FilmorateApplicationTests {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final UserDbStorage userStorage;
+
+    @Autowired
+    public FilmorateApplicationTests(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        userStorage = new UserDbStorage(jdbcTemplate);
+    }
+    @Test
+    public void testFindUserById() {
+        System.out.println(userStorage.getUserById(1));
+        Optional<User> userOptional = Optional.ofNullable(userStorage.getUserById(1));
+
+        assertThat(userOptional)
+              .isPresent()
+           .hasValueSatisfying(user ->
+                 assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+           );
+    }
+    /* private static final int PORT = 8080;
 
     public Gson gson = getGson();
 
@@ -294,5 +329,5 @@ class FilmorateApplicationTests {
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         assertEquals(400, response.statusCode());
-    }
+    }*/
 }
