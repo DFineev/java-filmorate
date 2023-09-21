735ff5debbc68251006d7fc8063a597c173c00f6
DFineev <fineev.mitya@yandex.ru>
2023-09-21 09:33:20 +0300

    change annotations

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
@@ -9,7 +10,7 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.*;
 
-@Component
+@Qualifier
 @Service
 @Slf4j
 public class UserService {
--- src/main/java/ru/yandex/practicum/filmorate/storage/DAO/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DAO/UserDbStorage.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.storage.DAO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
