b584eee4acfaedb6028f98e71756e609e93f4e2b
DFineev <fineev.mitya@yandex.ru>
2023-09-27 16:33:50 +0300

    add first DAO test.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -3,11 +3,18 @@ package ru.yandex.practicum.filmorate;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.DAO.UserDbStorage;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
-//@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 
 public class FilmorateApplicationTests {
@@ -16,7 +23,7 @@ public class FilmorateApplicationTests {
 
     @Test
     public void shouldFindUserById() {
-/*
+
         User user = new User("mail@mail.ru", "dolore", "Nick Name",
                 LocalDate.of(1946, 8, 20));
         Integer id = userStorage.createUser(user).getId();
@@ -24,9 +31,9 @@ public class FilmorateApplicationTests {
         Optional<User> userOptional = Optional.ofNullable(userStorage.getUserById(id));
 
         assertThat(userOptional)
-              .isPresent()
-            .hasValueSatisfying(u ->
-                  assertThat(u).hasFieldOrPropertyWithValue("id", id)
-         );*/
+                .isPresent()
+                .hasValueSatisfying(u ->
+                        assertThat(u).hasFieldOrPropertyWithValue("id", id)
+                );
     }
 }
